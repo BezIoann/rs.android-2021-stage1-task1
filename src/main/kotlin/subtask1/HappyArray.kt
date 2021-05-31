5a70528d721b16a88c147c20b710e14d3021a9e7
Ioann <42849417+BezIoann@users.noreply.github.com>
2021-05-31 23:20:32 +0300

    fix

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,28 +1,30 @@
 package subtask1
 
 class HappyArray {
-
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        val arr = sadArray.toMutableList()
-        var length = arr.size-1;
-//        println(length)
-        var k = true
-        while (k) {
-            k = false
-            for ( i in 0..length) {
-                if ( i > 0 && i != length ) {
-                    println("index " + i)
-                    if ( arr[i] > (arr[i+1] + arr[i-1]) ) {
-                        k = true;
-                        arr.removeAt(i)
-                        length = arr.size-1;
-                    }
+        if (sadArray.size <= 2) {
+            return sadArray
+        }
+        var sadArray2 = sadArray
+        val newArray = arrayListOf(sadArray[0])
+        var left: Int
+
+        while (true) {
+            left = 0
+            for (i in 1 until sadArray2.lastIndex) {
+                if (sadArray2[i] <= (sadArray2[left] + sadArray2[i + 1])) {
+                    newArray.add(sadArray2[i])
+                    left = i
                 }
             }
+            newArray.add(sadArray2[sadArray2.lastIndex])
+            if (newArray.size < sadArray2.size) {
+                sadArray2 = newArray.toIntArray()
+                newArray.clear()
+                newArray.add(sadArray2[0])
+            } else {
+                return newArray.toIntArray()
+            }
         }
-
-        println(arr)
-        return arr.toIntArray()
     }
 }
