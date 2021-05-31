216ebad3701aec90cd9a8d36c4d490229745c08b
Ioann <42849417+BezIoann@users.noreply.github.com>
2021-05-31 23:21:25 +0300

    fix

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -1,17 +1,16 @@
 package subtask2
 
 class BillCounter {
-
-    // TODO: Complete the following function
-    // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        var sum = 0
-        for ( i in 0..bill.size - 1 ) {
-            if (i != k) {
-                sum += bill[i]
+        val annaBill = (bill.sum() - bill[k]) / 2
+        return if (annaBill == b) {
+            "Bon Appetit"
+        } else {
+            if (annaBill >= b) {
+                "${annaBill - b}"
+            } else {
+                "${b - annaBill}"
             }
         }
-        var result = sum/2
-        return if (result == b) "Bon Appetit" else (b - result).toString();
     }
 }
